78578eb5000868f061bd87731e1bdea3fe524089
Tomas <lesicko.tomas@gmail.com>
2019-10-16 19:20:35 +0200

    mapping works with JS script

--- project/references/processRefs.js
+++ project/references/processRefs.js
@@ -1,42 +1,125 @@
 'use strict';
 
-(function() {
+// (function() {
 // temporary test references, references JSON will likely need to be online, accessing local files
 // in JavaScript is apparently very difficult for security reasons
-    var references = [
-        {
-            "document": { "document": "n4140", "section": "13.3:3.1" },
-            "semantics": { "file": "semantics/cpp/language/execution/stmt/try.k", "line": "222" }
-        },
-        {
-            "document": { "document": "n4140", "section": "13.3:3.2" },
-            "semantics": { "file": "semantics/cpp/language/execution/stmt/try.k", "line": "229" }
-        }];
 
-    var sectionNamesDict = {};
 
-    var addPair = function(k, v) {
+// var references = [
+//         {
+//             "document": { "document": "n4140", "section": "14.3:3.1" },
+//             "semantics": { "file": "semantics/cpp/language/execution/stmt/try.k", "line": "222" }
+//         },
+//         {
+//             "document": { "document": "n4140", "section": "14.3:3.3" },
+//             "semantics": { "file": "semantics/cpp/language/execution/stmt/try.k", "line": "229" },
+//         },
+//     {
+//         "document": { "document": "n4140", "section": "13.1:4.1" },
+//         "semantics": { "file": "semantics/cpp/language/execution/stmt/try.k", "line": "222" }
+//     },
+//     {
+//         "document": { "document": "n4140", "section": "30.10:3" },
+//         "semantics": { "file": "semantics/cpp/language/execution/stmt/try.k", "line": "222" }
+//     }];
+
+var sectionNamesDict = {};
+
+var addPair = function(k, v) {
         sectionNamesDict[k] = v;
-    };
+};
 
-    var storeRefsAndNames = function(refs, names) {
-        localStorage.setItem('references', JSON.stringify(refs));
-        localStorage.setItem('sectionNamesDict', JSON.stringify(names));
-    };
-
-    var processRefs = function() {
-        $.get("../section_names.txt", function(data) {
-            var array = data.split('\n');
-
-            for(var i = 0; i < array.length; i++) {
-                var keyValuePair = array[i].split(" ");
-
-                addPair(keyValuePair[0].slice(0, -1), keyValuePair[1]);
-            }
-
-            storeRefsAndNames(references, sectionNamesDict);
-        });
-    };
+var storeRefsAndNames = function(names) {
+        //sessionStorage.setItem('references', JSON.stringify(refs));
+        sessionStorage.setItem('dictionary', JSON.stringify(names));
+};
 
 
-}());
+// var processRefs = function() {
+//         $.get("../section_names.txt", function(data) {
+//             var array = data.split('\n');
+//
+//             for(var i = 0; i < array.length; i++) {
+//                 var keyValuePair = array[i].split(" ");
+//
+//                 addPair(keyValuePair[0].slice(0, -1), keyValuePair[1]);
+//             }
+//
+//             storeRefsAndNames(references, sectionNamesDict);
+//         });
+// };
+
+// var processRefs = function() {
+//
+//     sectionNamesDict[13.3] = "except.handle";
+//
+//     // $.get("../section_names.txt", function(data) {
+//     //     aa = data;
+//     //     array = data.split('\n');
+//     // });
+//
+//     for(var i = 0; i < array.length; i++) {
+//         var keyValuePair = array[i].split(" ");
+//
+//         addPair(keyValuePair[0].slice(0, -1), keyValuePair[1]);
+//     }
+//     storeRefsAndNames(references, sectionNamesDict);
+//
+// };
+
+var processRefs = function() {
+
+    //var array1 = ["a"];
+
+    // $.get("/home/tomas/WebstormProjects/test2/section_names.txt", function(data) {
+    // // $.get("../section_names.txt", function(data) {
+    //     array = data.split('\n');
+    // });
+    jQuery.get('https://raw.githubusercontent.com/TomasLesicko/Bachelor-Thesis/master/project/references/references.json', function(data) {
+        sessionStorage.setItem('references', data);
+    });
+
+    jQuery.get('https://raw.githubusercontent.com/TomasLesicko/Bachelor-Thesis/master/project/references/section_names_n4820.txt', function(data) {
+        var array1 = data.split('\n');
+
+        for(var i = 0; i < array1.length; i++) {
+            var keyValuePair = array1[i].split(" ");
+
+            addPair(keyValuePair[0].slice(0, -1), keyValuePair[1]);
+        }
+        storeRefsAndNames(sectionNamesDict);
+    });
+
+    jQuery.get('https://raw.githubusercontent.com/TomasLesicko/Bachelor-Thesis/master/project/section_mapping/section_mapping_to_n4820.json', function(data) {
+        sessionStorage.setItem('mapping', data);
+    });
+
+
+    // for(var i = 0; i < array.length; i++) {
+    //     var keyValuePair = array[i].split(" ");
+    //
+    //     addPair(keyValuePair[0].slice(0, -1), keyValuePair[1]);
+    // }
+    // storeRefsAndNames(references, sectionNamesDict);
+};
+
+
+var debugPlaceholder = 4;
+    // var processRefs = function() {
+    //     $.get("../section_names.txt", function(data) {
+    //         var array = data.split('\n');
+    //
+    //         for(var i = 0; i < array.length; i++) {
+    //             var keyValuePair = array[i].split(" ");
+    //
+    //             addPair(keyValuePair[0].slice(0, -1), keyValuePair[1]);
+    //         }
+    //
+    //         $.get("../referencesTest.json", function(data2) {
+    //             storeRefsAndNames(data2, sectionNamesDict);
+    //         });
+    //
+    //     });
+    // };
+
+//}());
--- project/references/script.js
+++ project/references/script.js
@@ -1,18 +1,55 @@
-// need to get rid of these global variables
-var dictionaryStr = localStorage.getItem('dictionary');
-var referencesStr = localStorage.getItem('references');
-var dictionary = JSON.parse(dictionaryStr);
-var references = JSON.parse(referencesStr);
+
 
 var colorReferencedBlocks = function() {
+
+    // var dictionaryStr = sessionStorage.getItem('dictionary');
+    // var referencesStr = sessionStorage.getItem('references');
+    var dictionary = JSON.parse(sessionStorage.getItem('dictionary'));
+    var references = JSON.parse(sessionStorage.getItem('references'));
+    var mapping = JSON.parse(sessionStorage.getItem('mapping'));
+
+    // for(var i = 0; i < references.length; i++) {
+    //     var deb = typeof(references);
+    //         var sectionInfo = references[i].document.section.split(':');
+    //
+    //         var HTMLpage = dictionary[sectionInfo[0]]+".html";
+    //         var thisPage = location.pathname.split("/").slice(-1);
+    //
+    //         if(HTMLpage === thisPage[0]) {
+    //             if(references[i].document.document === "n4820") {
+    //                 document.getElementById(sectionInfo[1]).style.backgroundColor = "#009933";
+    //             } else {
+    //                 var mappedSectionInfo = mapping[references[i].document.document][sectionInfo[0]];
+    //
+    //                 document.getElemefindntById(sectionInfo[1]).style.backgroundColor = "#66ff66";
+    //             }
+    //
+    //         }
+    // }
+
     for(var i = 0; i < references.length; i++) {
-            var sectionInfo = references[i].document.section.split(':');
+        var sectionInfo = references[i].document.section.split(':');
+        if (sectionInfo[0] === "5.16") {
+            var deb = 4;
+        }
 
-            var HTMLpage = dictionary[sectionInfo[0]]+".html";
-            var thisPage = location.pathname.split("/").slice(-1);
+        //var HTMLpage = dictionary[sectionInfo[0]]+".html";
+        var thisPage = location.pathname.split("/").slice(-1);
+        var mappedSectionInfo;
+        var color;
+        if(references[i].document.document !== "n4820") {
+            mappedSectionInfo = mapping[references[i].document.document][sectionInfo[0]];
+            color = "#66ff66"
+        } else {
+            mappedSectionInfo = sectionInfo[0];
+            color = "#009933";
+        }
 
-            if(HTMLpage === thisPage[0]) {
-                document.getElementById(sectionInfo[1]).style.backgroundColor = "green";
-            }
+        var HTMLpage = dictionary[mappedSectionInfo]+".html";
+
+
+        if(HTMLpage === thisPage[0]) {
+            document.getElementById(sectionInfo[1]).style.backgroundColor = color;
+        }
     }
 };
