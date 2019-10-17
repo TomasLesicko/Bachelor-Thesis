3de7203fb0ab6628c64b10a50c523f494e7de1a6
Tomas <lesicko.tomas@gmail.com>
2019-10-17 17:09:36 +0200

    section mapping uses all tags mentioned in references

--- project/section_mapping/section_mapping.py
+++ project/section_mapping/section_mapping.py
@@ -1,23 +1,27 @@
 import json
 import re
-
-revisions = ["n4296", "n4778"]
-regex_text_section_line = '([A-Z0-9](?:\d)*(?:\.\d+)*): (\S+) - (?:[^\n]+)\n'
+from urllib.request import urlopen
 
 
-def read_current_revision_sections(current):
+def extract_revision_tag_list_from_references(revision_set):
+    with urlopen("https://raw.githubusercontent.com/TomasLesicko/Bachelor-Thesis/master/project/references/references.json") as url:
+        references = json.loads(url.read())
+        for reference in references:
+            revision_set.add(reference['document']['document'].lower())
+
+
+def read_current_revision_sections(regex_text_section_line, current):
     with open("section_names_n4820.txt", 'r') as sections_text:
         text = sections_text.read()
         matches = re.findall(regex_text_section_line, text)
         for tuple in matches:
             current[tuple[1]] = tuple[0]
             
-            
-def map_revision_sections(revisions, current, older_revision_sections):
+def map_revision_sections(regex_text_section_line, revision_set, current, older_revision_sections):
     file_name = "section_names_"
     file_suffix = ".txt"
     
-    for revision_tag in revisions:
+    for revision_tag in revision_set:
         
         with open(file_name + revision_tag + file_suffix, 'r') as sections_text:
             text = sections_text.read()
@@ -29,37 +33,21 @@ def map_revision_sections(revisions, current, older_revision_sections):
             older_revision_sections[revision_tag] = this_revision_sections
 
 
-# with open("section_names_n4296.txt", 'r') as sections_text:
-    # text = sections_text.read()
-    # r = re.findall(regex_text_section_line, text)
-    # this_revision_sections = {}
-    # # for tuple in r:
-        # # dicti[tuple[0]] = tuple[1]
-    # # rs = json.dumps(dict(r))
-    # for tuple in r:
-        # maps_to = current_revision_sections.get(tuple[1])
-        # print(tuple[1] + " maps to " + (maps_to if maps_to else "None"))
-        # this_revision_sections[tuple[0]] = maps_to
-    # older_revision_sections["n4296"] = this_revision_sections
-
-
-#with open("section_names_n4778.txt", 'r') as sections_text:
-#    text = sections_text.read()
-#    r = re.findall(regex_text_section_line, text)
-#    sections_dict["n4778"] = dict(r)
-
-
 def write_mapping(older_revision_sections):
     with open("section_mapping_to_n4820.json", 'w') as sm:
         json.dump(older_revision_sections, sm, indent = 4)
 
 
 def main():
+    revision_set = set()
     current = {}
     older_revision_sections = {}
+    
+    regex_text_section_line = '([A-Z0-9](?:\d)*(?:\.\d+)*): (\S+) - (?:[^\n]+)\n'
 
-    read_current_revision_sections(current)
-    map_revision_sections(revisions, current, older_revision_sections)
+    extract_revision_tag_list_from_references(revision_set)
+    read_current_revision_sections(regex_text_section_line, current)
+    map_revision_sections(regex_text_section_line, revision_set, current, older_revision_sections)
     write_mapping(older_revision_sections)
 
 if __name__ == "__main__":
