e9aacecc0365748a85738277fa55c421960200fd
Tomas <lesicko.tomas@gmail.com>
2020-02-24 14:38:08 +0100

    tidy up script.js

--- project/references/script.js
+++ project/references/script.js
@@ -1,51 +1,30 @@
-
+'use strict';
 
 var colorReferencedBlocks = function() {
 
-    // var dictionaryStr = sessionStorage.getItem('dictionary');
-    // var referencesStr = sessionStorage.getItem('references');
     var dictionary = JSON.parse(sessionStorage.getItem('dictionary'));
     var references = JSON.parse(sessionStorage.getItem('references'));
     var mapping = JSON.parse(sessionStorage.getItem('mapping'));
 
-    // for(var i = 0; i < references.length; i++) {
-    //     var deb = typeof(references);
-    //         var sectionInfo = references[i].document.section.split(':');
-    //
-    //         var HTMLpage = dictionary[sectionInfo[0]]+".html";
-    //         var thisPage = location.pathname.split("/").slice(-1);
-    //
-    //         if(HTMLpage === thisPage[0]) {
-    //             if(references[i].document.document === "n4820") {
-    //                 document.getElementById(sectionInfo[1]).style.backgroundColor = "#009933";
-    //             } else {
-    //                 var mappedSectionInfo = mapping[references[i].document.document][sectionInfo[0]];
-    //
-    //                 document.getElemefindntById(sectionInfo[1]).style.backgroundColor = "#66ff66";
-    //             }
-    //
-    //         }
-    // }
-
+    var thisPage = location.pathname.split("/").slice(-1);
 
     var faultyReferenceFormatCounter = 0;
     for(var i = 0; i < references.length; i++) {
-        var sectionInfo = references[i].document.section.split(/[:\/]+/); // supports 1.2:3 and 1.2/3
+        var section = references[i].document.section.split(/[:\/]+/); // supports 1.2:3 and 1.2/3
 
-        if (typeof(sectionInfo) == 'undefined' || sectionInfo.length < 2) {
+        if (typeof(section) == 'undefined' || section.length < 2) {
             ++faultyReferenceFormatCounter;
             continue;
         }
 
-        var thisPage = location.pathname.split("/").slice(-1);
         var mappedSectionInfo;
         var color;
 
         if(references[i].document.document !== "n4820") {
-            mappedSectionInfo = mapping[references[i].document.document.toLowerCase()][sectionInfo[0]];
+            mappedSectionInfo = mapping[references[i].document.document.toLowerCase()][section[0]];
             color = "#66ff66";
         } else {
-            mappedSectionInfo = sectionInfo[0];
+            mappedSectionInfo = section[0];
             color = "#009933";
         }
 
@@ -53,7 +32,7 @@ var colorReferencedBlocks = function() {
 
 
         if(HTMLpage === thisPage[0]) {
-            var blocks = sectionInfo[1].split('-');
+            var blocks = section[1].split('-');
             for (var j = 0; j < blocks.length; ++j) {
                 document.getElementById(blocks[j]).style.backgroundColor = color;
 
@@ -61,8 +40,6 @@ var colorReferencedBlocks = function() {
                 document.getElementById(blocks[j]).setAttribute('title', "file: " +
                     references[i].semantics.file + " ,line: " + references[i].semantics.line);
             }
-
-            // document.getElementById(sectionInfo[1]).style.backgroundColor = color;
         }
 
         if (i === references.length - 2) {
