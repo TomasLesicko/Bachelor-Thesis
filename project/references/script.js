5c866a69aec9b7e04a0e81b9fb87839eee3746a2
Tomas <lesicko.tomas@gmail.com>
2020-02-26 12:16:29 +0100

    refactored and modularized

--- project/references/script.js
+++ project/references/script.js
@@ -1,75 +1,91 @@
 (function() {
     "use strict";
 
-    const currentRevision = "n4820";
+    const currentRevisionTag = "n4820";
 
-    var dictionary = JSON.parse(sessionStorage.getItem('dictionary'));
-    var references = JSON.parse(sessionStorage.getItem('references'));
-    var mapping = JSON.parse(sessionStorage.getItem('mapping'));
+    const currentRevisionTagColor = "#009933";
+    const differentRevisionTagColor = "#66ff66";
 
-    var thisPage = location.pathname.split("/").slice(-1);
-    var faultyReferenceFormatCounter = 0;
+    let chapterNameDict = JSON.parse(sessionStorage.getItem('dictionary'));
+    let references = JSON.parse(sessionStorage.getItem('references'));
+    let chapterMapping = JSON.parse(sessionStorage.getItem('mapping'));
+
+    let thisHTMLPageName = location.pathname.split("/").slice(-1)[0];
+    let faultyReferenceFormatCounter = 0;
 
 
-    function isValidReference(section) {
-        /*
-        section must be a valid value and consist of a chapter index
-        and at least one section id
-         */
-        return section && section.length >= 2;
+    function colorRelevantSections(revisionTag, sectionsToColor) {
+        sectionsToColor.forEach((section) =>
+            document.getElementById(section).style.backgroundColor =
+                getHighlightColor(revisionTag));
     }
 
-    function getReferenceComment(reference) {
-        return reference.semantics.file + ", line: " +
-            reference.semantics.line;
+    function displaySemanticsComments(semantics, commentedSections) {
+        commentedSections.forEach((section) =>
+            document.getElementById(section).title =
+                getReferenceComment(semantics));
+    }
+
+    function getHighlightColor(revisionTag) {
+        return (revisionTag === currentRevisionTag) ?
+            currentRevisionTagColor : differentRevisionTagColor;
+    }
+
+    function getReferenceComment(semantics) {
+        return semantics.file + ", line: " + semantics.line;
     }
 
     function handleFaultyReferences() {
-        //TODO different alerts for diferent problems
+        //TODO different alerts for different problems
         if (faultyReferenceFormatCounter > 0) {
-            alert(faultyReferenceFormatCounter + " faulty reference formats found.")
+            alert(faultyReferenceFormatCounter +
+                " faulty reference formats found.")
         }
     }
 
     function highlightReferencedSections() {
         references.forEach((reference) => {
-            highlightRelevantSection(reference, dictionary, mapping);
+            highlightRelevantSection(reference);
         });
 
         handleFaultyReferences();
     }
 
-    function highlightRelevantSection(reference, dictionary, mapping) {
-        var section = reference.document.section.split(/[:\/]+/); // supports 1.2:3 and 1.2/3
+    function highlightRelevantSection(reference) {
+        // supports 1.2:3 and 1.2/3
+        let chapterAndSections = reference.document.section.split(/[:\/]+/);
 
-        if (!isValidReference(section)) {
+        let currentRevisionChapter;
+        let HTMLPageName;
+
+        if (!isValidReference(chapterAndSections)) {
             ++faultyReferenceFormatCounter;
             return;
         }
 
-        var mappedSectionInfo;
-        var color;
+        currentRevisionChapter = mapChapterToCurrentRevision(
+            reference.document.document, chapterAndSections[0]);
+        HTMLPageName = chapterNameDict[currentRevisionChapter]+".html";
 
-        if(reference.document.document === currentRevision) {
-            mappedSectionInfo = section[0];
-            color = "#009933";
-        } else {
-            mappedSectionInfo = mapping[reference.document.document.toLowerCase()][section[0]];
-            color = "#66ff66";
+        if (HTMLPageName === thisHTMLPageName) {
+            let sections = chapterAndSections[1].split("-");
+
+            colorRelevantSections(reference.document.document, sections);
+            displaySemanticsComments(reference.semantics, sections);
         }
+    }
 
-        var HTMLpage = dictionary[mappedSectionInfo]+".html";
+    function isValidReference(chapterAndSection) {
+        /*
+        section must be a valid value and consist of a chapter index
+        and at least one section id
+         */
+        return chapterAndSection && chapterAndSection.length >= 2;
+    }
 
-
-        if(HTMLpage === thisPage[0]) {
-            var blocks = section[1].split('-');
-            for (var j = 0; j < blocks.length; ++j) {
-                document.getElementById(blocks[j]).style.backgroundColor = color;
-
-                //semantics comments
-                document.getElementById(blocks[j]).title = getReferenceComment(reference);
-            }
-        }
+    function mapChapterToCurrentRevision(revisionTag, chapter) {
+        return (revisionTag === currentRevisionTag) ?
+            chapter : chapterMapping[revisionTag.toLowerCase()][chapter];
     }
 
 
