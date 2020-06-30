4fe76d599b98e5ecb783ab03c4dc91b575c0be8f
Tomas <lesicko.tomas@gmail.com>
2020-06-30 21:27:38 +0200

    revision_PDF_to_txt fixes

--- project/tools/revision_PDF_to_txt.py
+++ project/tools/revision_PDF_to_txt.py
@@ -1,5 +1,6 @@
 import sys
 import re
+sys.path.append("../section_mapping")
 from paragraph_mapping import read_referenced_revisions
 
 
@@ -15,24 +16,24 @@ def save_txt_revisions(txt_revisions):
 
 
 def is_valid_arg(arg):
-    tag_regex = re.compile(r"n\d{4}(?:\.pdf)?", re.IGNORECASE)
-    return tag_regex.match(re.escape(arg))
+    tag_regex = re.compile(r"^n\d{4}(?:\.pdf)?$", re.IGNORECASE)
+    return tag_regex.match(arg)
 
 
 def main(argv):
     revision_set = set()
 
-    for i in range(1, len(argv)):
+    for i in range(2, len(argv)):
         if not is_valid_arg(argv[i]):
-            print("Invalid tag: %s" % argv[1])
+            print("Invalid tag: %s" % argv[i])
         else:
             revision_set.add(argv[i].lower().split(".")[0])
 
     if not revision_set:
-        print("Usage: revision_PDF_to_text.py [revision tags]\n"
-              " e.g. \"revision_PDF_to_text.py n4140 N4296 n4800.pdf\"")
+        print("Usage: revision_PDF_to_text.py <port number> [revision tags]\n"
+              " e.g. \"revision_PDF_to_text.py 9997 n4140 N4296 n4800.pdf\"")
     else:
-        txt_revisions = read_referenced_revisions(revision_set)
+        txt_revisions = read_referenced_revisions(revision_set, argv[1])
         save_txt_revisions(txt_revisions)
 
 
